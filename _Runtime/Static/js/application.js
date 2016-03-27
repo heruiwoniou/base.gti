a757b1fdc07320ba12eb4937cba579d555d42fcf
heruiwoniou <heruiwoniou@qq.com>
2016-03-27 19:02:33 +0800

    'base'

--- _Runtime/Static/js/application.js
+++ _Runtime/Static/js/application.js
@@ -4,6 +4,11 @@
 define(function(){
     return {
         init:function(){
+
+            var json_string="{a:1}";
+            var reg=/(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)('")?:/g;
+            json_string=json_string.replace(reg,'$1"$3":');
+            console.log(json_string);
         }
     }
 })
